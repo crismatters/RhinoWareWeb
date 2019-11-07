90bc95fe7a776b0360271dacf42d6c9ad9896538
crismatters <crismatters@localhost.localdomain>
2019-11-06 21:48:06 -0600

    Servicios

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,9 +6,11 @@ const Products = () => {
             <h2 className="center">Productos</h2>
             <section className="gallery">
                 <div className="third columns half-fixed item" style={{ background: 'linear-gradient(0deg, rgb(8, 174, 234) 0%, rgb(42, 245, 152) 100%)' }}>
-                    <div className="label">CRM Rhino</div>
+                    // <div className="label">CRM Rhino</div>
                     {/* <p>Web App moderna perfecta para aquellos que quieran trabajar con su información en tiempo real. Ideal para pequenas y medianas empresas.</p> */}
-                
+                    <div className="label">Puntos De Venta</div>
+                    <div className="label">Sistemas De Administración</div>
+                    <div className="label">CRM Rhino</div>
                 </div>
             </section>
             <div className="space"></div>
